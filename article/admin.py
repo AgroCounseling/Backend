3ce43386bca7d0ed2f838eaf315ab4f5243e48ca
SultanFranco98 <s.syidaliev@gmail.com>
2020-08-11 22:46:15 +0600

    added mark_safe method on article admin

--- article/admin.py
+++ article/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 from django.utils.translation import gettext_lazy as _
+from django.utils.safestring import mark_safe
 
 admin.site.site_header = _("Агро Консультирование")
 admin.site.site_title = _("Агро Консультирование")
@@ -18,14 +19,18 @@ class AdditionInline(admin.TabularInline):
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    fields = ['user', 'category', 'subcategory', 'types', 'subtypes', 'title_ru', 'title_kg', 'text_ru', 'text_kg', 'status', 'votes', 'pub_date']
+    fields = ['user', 'category', 'subcategory', 'types', 'subtypes', 'title_ru', 'title_kg', 'article_text_ru', 'text_kg', 'status', 'votes', 'pub_date']
     list_display = ['title', 'status', 'category', 'user', 'votes']
     list_filter = ['user__email', 'user__first_name', 'user__last_name', 'status', 'category', 'subcategory', 'types',
                    'subtypes', 'votes']
     search_fields = ['user__email', 'user__first_name', 'user__last_name']
-    readonly_fields = ['pub_date', 'votes']
+    readonly_fields = ['pub_date', 'votes', 'article_text_ru']
     ordering = ['-pub_date']
     inlines = [AdditionInline]
+
+    def article_text_ru(self, obj):
+        return mark_safe(obj.text_ru)
+    article_text_ru.short_description = 'Текст [ru]'
 
 
 class VoteAdmin(admin.ModelAdmin):
